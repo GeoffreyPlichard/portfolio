5c66bfd9a315ee01df57af2fd016be7e4abf8e7c
Geoffrey P <geoffreyplichard@gmail.com>
2014-11-23 15:29:21 +0100

    Timeline ok

--- index.php
+++ index.php
@@ -8,13 +8,72 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
-
 	<link rel="stylesheet" href="css/main.css">
+
+	<script src="js/vendor/modernizr.js"></script> <!-- Modernizr -->
 </head>
 <body>
 	<!--[if lt IE 8]>
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
+
+    <header style="height:500px;">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </header>
+
+	<div class="container-wrap">
+	    <section class="timeline container">
+	    	<div class="timeline-block">
+	    		<div class="timeline-img">
+	    			<img src="img/ecole_multimedia.jpg" alt="#">
+	    		</div>
+	    		<div class="timeline-content">
+	    			<h2>Title of section 1</h2>
+	    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi culpa omnis dolore. Fugit molestias accusantium qui unde, nam ducimus dicta, maiores culpa placeat sint vero amet quis, officia id repudiandae.</p>
+	    			<a class="read-more" href="#">Read more</a>
+	    			<span class="date">Jan 14</span>
+	    		</div>
+	    	</div>
+	    	<div class="timeline-block">
+	    		<div class="timeline-img">
+	    			<img src="" alt="#">
+	    		</div>
+	    		<div class="timeline-content">
+	    			<h2>Title of section 1</h2>
+	    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi culpa omnis dolore. Fugit molestias accusantium qui unde, nam ducimus dicta, maiores culpa placeat sint vero amet quis, officia id repudiandae.</p>
+	    			<a class="read-more" href="#">Read more</a>
+	    			<span class="date">Jan 14</span>
+	    		</div>
+	    	</div>
+	    	<div class="timeline-block">
+	    		<div class="timeline-img">
+	    			<img src="" alt="#">
+	    		</div>
+	    		<div class="timeline-content">
+	    			<h2>Title of section 1</h2>
+	    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi culpa omnis dolore. Fugit molestias accusantium qui unde, nam ducimus dicta, maiores culpa placeat sint vero amet quis, officia id repudiandae.</p>
+	    			<a class="read-more" href="#">Read more</a>
+	    			<span class="date">Jan 14</span>
+	    		</div>
+	    	</div>
+	    	<div class="timeline-block">
+	    		<div class="timeline-img">
+	    			<img src="" alt="#">
+	    		</div>
+	    		<div class="timeline-content">
+	    			<h2>Title of section 1</h2>
+	    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi culpa omnis dolore. Fugit molestias accusantium qui unde, nam ducimus dicta, maiores culpa placeat sint vero amet quis, officia id repudiandae.</p>
+	    			<a class="read-more" href="#">Read more</a>
+	    			<span class="date">Jan 14</span>
+	    		</div>
+	    	</div>
+	    </section>
+	</div>
 
     <h1>TEST DE LA FONT</h1>
 
